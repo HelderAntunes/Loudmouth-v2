57a487eab27f56b14a0bfb89a8f058a984f46ac9
Hélder Antunes <helder_antunes-@hotmail.com>
2017-05-23 14:51:32 +0100

    Added useful functions to database.

--- src/server/Server.java
+++ src/server/Server.java
@@ -2,19 +2,23 @@ package server;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.ArrayList;
 import java.util.concurrent.ConcurrentHashMap;
 
 import com.sun.net.httpserver.*;
 
 public class Server {
 
-    private ConcurrentHashMap<String, String> user_passord = new ConcurrentHashMap<>();
+    private ConcurrentHashMap<String, String> user_password = new ConcurrentHashMap<>();
+    private ConcurrentHashMap<String, ArrayList<String>> user_chats = new ConcurrentHashMap<>();
+    private ConcurrentHashMap<String, ArrayList<String>> user_invitations = new ConcurrentHashMap<>();
+    private ConcurrentHashMap<String, ArrayList<ArrayList<String>>> chat_messages = new ConcurrentHashMap<>(); // chat -> [[author, message, date], ...]
 
     public static void main(String[] args) throws Exception {
         new Server();
     }
 
-    public Server() throws IOException {
+    private Server() throws IOException {
         HttpServer httpServer = HttpServer.create(new InetSocketAddress(8000), 0);
         Handler handler = new Handler(this);
 
@@ -34,23 +38,108 @@ public class Server {
         httpServer.start();
     }
 
-    public boolean insertUser(String username, String password) {
-        if (user_passord.containsKey(username))
+    void insertUser(String username, String password) {
+        if (user_password.containsKey(username))
+            return;
+        user_password.put(username, password);
+    }
+
+    boolean userExist(String username, String password) {
+        String password_ = user_password.get(username);
+        return password.equals(password_);
+    }
+
+    boolean userExist(String username) {
+        return user_password.containsKey(username);
+    }
+
+    boolean insertChat(String name) {
+        if (chat_messages.containsKey(name))
             return false;
-        user_passord.put(username, password);
+        chat_messages.put(name, new ArrayList<>());
         return true;
     }
 
-    public boolean userExist(String username, String password) {
-        String password_ = user_passord.get(username);
-        if (password.equals(password_))
-            return true;
-        return false;
+    boolean chatExist(String name) {
+        return chat_messages.containsKey(name);
     }
 
-    public boolean userExist(String username) {
-        if (user_passord.containsKey(username))
+    ArrayList<ArrayList<String>> getChatMessages(String chatName) {
+        return chat_messages.get(chatName);
+    }
+
+    void addMessageToChat(String chatName, ArrayList<String> message) {
+        ArrayList<ArrayList<String>> messages = chat_messages.get(chatName);
+        messages.add(message);
+    }
+
+    boolean addUserToChat(String username, String chatName) {
+        return insertInHashMap(user_chats, username, chatName);
+    }
+
+    ArrayList<String> getUserChats(String username) {
+        if (!user_chats.containsKey(username))
+            return new ArrayList<>();
+        return user_chats.get(username);
+    }
+
+    boolean leaveChat(String username, String chatName) {
+        ArrayList<String> chats = getUserChats(username);
+        if (chats.contains(chatName)) {
+            chats.remove(chatName);
             return true;
-        return false;
+        }
+        else {
+            return false;
+        }
+    }
+
+    boolean insertInvitation(String chatName, String username) {
+        return insertInHashMap(user_invitations, username, chatName);
+    }
+
+    ArrayList<String> getUserInvitations(String username) {
+        if (!user_invitations.containsKey(username))
+            return new ArrayList<>();
+        return user_invitations.get(username);
+    }
+
+    boolean acceptInvitation(String username, String chatName) {
+        ArrayList<String> userInvitations = getUserInvitations(username);
+        if (userInvitations.contains(chatName)) {
+            userInvitations.remove(chatName);
+            addUserToChat(username, chatName);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    boolean declineInvitation(String username, String chatName) {
+        ArrayList<String> userInvitations = getUserInvitations(username);
+        if (userInvitations.contains(chatName)) {
+            userInvitations.remove(chatName);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    boolean insertInHashMap(ConcurrentHashMap<String, ArrayList<String>> hashMap, String key, String value) {
+        if (hashMap.containsKey(key)) {
+            ArrayList<String> currInv = hashMap.get(key);
+            if (currInv.contains(value))
+                return false;
+            currInv.add(value);
+            return true;
+        }
+        else {
+            hashMap.put(key, new ArrayList<>());
+            ArrayList<String> currChats = hashMap.get(key);
+            currChats.add(value);
+            return true;
+        }
     }
 }
