2599e771fd7ffb87625f7f060b2881a9a26ebe4e
Hélder Antunes <helder_antunes-@hotmail.com>
2017-05-26 19:07:39 +0100

    Change executor.

--- src/client/network/HttpClient.java
+++ src/client/network/HttpClient.java
@@ -1,9 +1,5 @@
 package client.network;
 
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -11,8 +7,6 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Base64;
-import java.util.Iterator;
-import java.util.Set;
 
 public class HttpClient {
 
